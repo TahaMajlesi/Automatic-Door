aed5fa94cb956c2c64c4bf082030fdaf255d2c1d
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2024-12-18 15:59:30 -0800

    update

---
+++ scripts/desktop/navbar.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#corporateDropDown').on('click', function() {
+        $('#corporateMenu').toggleClass('hidden');
+    });
+
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('#corporateDropDown').length) {
+            $('#corporateMenu').addClass('hidden');
+        }
+    });
+
+
+    $('#productsDropDown').on('click', function() {
+        $('#productsMenu').toggleClass('hidden');
+    });
+
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('#productsDropDown').length) {
+            $('#productsMenu').addClass('hidden');
+        }
+    });
+});
